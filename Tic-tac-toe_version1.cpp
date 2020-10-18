a19fedb5b199b8f5a7db1dbcb3a047633906e003
Anant mishra <69999889+Anant-coder@users.noreply.github.com>
2020-10-18 19:57:17 +0530

    Update Tic-tac-toe_version1.cpp

--- Tic-tac-toe_version1.cpp
+++ Tic-tac-toe_version1.cpp
@@ -20,6 +20,7 @@ int main()
     int x, y;
     int win = 0;
     int chance = 1;
+
     cout << endl;
     changecolor(15);
     cout << "Hello! Welcome to my Tic-Tac-Toe :)" << endl;
@@ -68,26 +69,26 @@ int placechar(char matrix[9], int chance)
     cout << "Enter the position : " << endl;
     cin >> x;
 
-    if (x <=9 && x > 0)
+    if (x <= 9 && x > 0)
     {
-        if (matrix[x-1] != 'O' && matrix[x-1] != 'X')
+        if (matrix[x - 1] != 'O' && matrix[x - 1] != 'X')
         {
             if (chance == 1)
             {
-                matrix[x-1] = 'X';
+                matrix[x - 1] = 'X';
                 chance = 2;
                 return chance;
             }
             else
             {
-                matrix[x-1] = 'O';
+                matrix[x - 1] = 'O';
                 chance = 1;
                 return chance;
             }
         }
         else
         {
-            cout << "Invalid !! choice  :( " << endl;
+            cout << "Hey ! it is filled already  :( " << endl;
             placechar(matrix, chance);
         }
     }
@@ -103,23 +104,23 @@ int checkwin(char matrix[9], int chance)
     int matrixcount = 0, condition = 0;
     for (int i = 0; i < 9; i++)
     {
-        matrixcount += (matrix[i] == 'X' && matrix[i] == 'O');
+        matrixcount += (matrix[i] == 'X' || matrix[i] == 'O');
     }
 
     // Check rows
-    for (int i = 0; i < 7; i+= 3)
+    for (int i = 0; i < 7; i += 3)
     {
-        if ((matrix[i] == 'X' || matrix[i] == 'O') && matrix[i] == matrix[i+1] && matrix[i] == matrix[i+2])
+        if ((matrix[i] == 'X' || matrix[i] == 'O') && matrix[i] == matrix[i + 1] && matrix[i] == matrix[i + 2])
             condition = 1;
     }
 
     // Check columns
     for (int i = 0; i < 3; i++)
     {
-        if ((matrix[i] == 'X' || matrix[i] == 'O') && matrix[i] == matrix[i+3] && matrix[i] == matrix[i+6])
+        if ((matrix[i] == 'X' || matrix[i] == 'O') && matrix[i] == matrix[i + 3] && matrix[i] == matrix[i + 6])
             condition = 1;
     }
-    
+
     // Check diagonals
     if ((matrix[0] == 'X' || matrix[0] == 'O') && matrix[0] == matrix[4] && matrix[0] == matrix[8])
         condition = 1;
@@ -141,7 +142,7 @@ int checkwin(char matrix[9], int chance)
         }
         return 1;
     }
-    else if (condition != 1 && matrixcount == 9)
+    else if (matrixcount == 9 && condition != 1)
     {
         cout << "Draw !";
         displaymatrix(matrix);
